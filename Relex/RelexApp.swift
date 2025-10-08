e98809e3fa9b223a5697600f4b2a210c5b561cac
TheoDepr <tdepsh@gmail.com>
2025-10-08 20:52:21 +0200

    Initial Commit

---
+++ Relex/RelexApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelexApp.swift
+//  Relex
+//
+//  Created by Theo Depraetere on 08/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct RelexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
